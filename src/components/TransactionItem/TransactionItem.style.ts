be83ef66b8d815f6d2924484788dc2c43865a8ca
Samir.mimoun <Samir.mimoun@altran.com>
2020-12-23 20:26:16 +0100

    transaction-list

---
+++ src/components/TransactionItem/TransactionItem.style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    borderBottomWidth: 0.5,
+    borderBottomColor: "black",
+  },
+  icon: {
+    height: 20,
+    width: 20,
+    alignSelf: "center",
+    tintColor: "white",
+    marginLeft: "2%",
+  },
+  recipient: { color: "white", fontSize: 12, opacity: 0.5 },
+  transactionTextContainer: { flex: 1, padding: "2%" },
+  value: { color: "white", fontSize: 15 },
+});
