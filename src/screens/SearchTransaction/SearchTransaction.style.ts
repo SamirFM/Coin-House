3fe55685db2f87565844a844d9f84c70649bddfe
SamirFM <SamirFM@users.noreply.github.com>
2020-12-23 18:50:34 +0100

    transaction search (#5)
    Co-authored-by: Samir.mimoun <Samir.mimoun@altran.com>

--- src/screens/SearchTransaction/SearchTransaction.style.ts
+++ src/screens/SearchTransaction/SearchTransaction.style.ts
@@ -1,7 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  input: { paddingBottom: "5%", paddingTop: "5%" },
+  labelInput: { color: "white", fontSize: 10 },
+  layout: { flex: 1, alignItems: "center", padding: "10%" },
+  searchTransactionContainer: { flex: 1 },
 });
